a80a018660a21dc5316b75caf9f3af73adfff527
trucdongtxtv <trucdong.txtv@gmail.com>
2021-08-25 09:00:56 +0700

    Demo app with GET

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+const express = require('express');
+const app = express();
   
-}).listen(8080);
+// Defining get request at '/' route
+app.get('/', (req, res) => {
+  res.json({
+    number: 1
+  });
+});
+  
+// Defining get request at '/multiple' route
+app.get('/multiple', (req, res) => {
+  res.json({
+    number: 1,
+    name: 'John',
+    gender: 'male'
+  });
+});
+  
+// Defining get request at '/array' route
+app.get('/array', (req, res) => {
+  res.json([{
+      number: 1,
+      name: 'John',
+      gender: 'male'
+    },
+    {
+      number: 2,
+      name: 'Ashley',
+      gender: 'female'
+    }
+  ]);
+});
+  
+// Setting the server to listen at port 3000
+app.listen(8000, (req, res) => {
+  console.log("Server is running at port 8000");
+});
