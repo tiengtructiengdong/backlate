0577c9acc924cf484981fbc1a321070bc4fdccca
trucdongtxtv <trucdong.txtv@gmail.com>
2021-08-25 14:16:46 +0700

    Play with HTTP status code

--- auth/index.js
+++ auth/index.js
@@ -1,12 +1,25 @@
-module.exports = app => {
+const authenticate = (username, password) => {
+  return {
+    token: "1fe5482djep23o1210saxecswm",
+  };
+};
 
-  app.get('/auth', (req, res) => {
+module.exports = (app) => {
+  app.get("/auth", (req, res) => {
     const username = req.query.username;
     const password = req.query.password;
 
-    res.json({
-      username: username,
-      password: password
-    });
+    if (username && password) {
+      res.json({
+        status: 200,
+        username: username,
+        ...authenticate(username, password)
+      });
+    } else {
+      res.json({
+        status: 400,
+        message: "Missing input!",
+      });
+    }
   });
-}
+};
--- vehicles/index.js
+++ vehicles/index.js
@@ -1,13 +1,12 @@
-module.exports = app => {
-  
-  app.get('/vehicle', (req, res) => {
-    const token = req.query.token
+module.exports = (app) => {
+  app.get("/vehicle", (req, res) => {
+    const token = req.query.token;
 
     res.json({
       number: 1,
-      name: 'John',
-      gender: 'male',
-      page: page
+      name: "John",
+      gender: "male",
+      page: page,
     });
   });
-}
+};
